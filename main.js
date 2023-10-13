bf04eb5f9ffcf779f8d16a2baaeb516f92c288ee
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-10-13 13:24:43 +0200

    /

--- main.js
+++ main.js
@@ -37,3 +37,35 @@ createForm.addEventListener('submit', (event) => {
 
 });
 
+function sendData(event){
+    event.preventDefault();
+
+    const form = document.forms['create'];
+
+    // const title = form['title'].value;
+
+    const formData = new FormData(form);
+
+    // let isOverBool;
+
+    // if (formData.get('isOver') === 'on') {
+    //     isOverBool = true;
+    // } else {
+    //     isOverBool = false;
+    // }
+
+    const newShow = {
+        title: formData.get('title'),
+        author: formData.get('author'),
+        imageUrl: formData.get('imageUrl'),
+        isOver: formData.get('isOver') === 'on' ? true : false,
+        upVotes: 0,
+        downVotes: 0
+    }
+
+    console.log(newShow);
+
+    DBService.createShow(newShow)
+    .then(show => window.location = './index.html')
+    .catch(error => alert(error.message));
+}
